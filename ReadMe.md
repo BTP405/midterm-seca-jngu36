3ce046f65348c6be3fcae1b7ffb82eeb192f5913
jngu36 <42044445+jngu36@users.noreply.github.com>
2024-03-07 23:07:43 +0000

    pushing answers

--- ReadMe.md
+++ ReadMe.md
@@ -7,3 +7,6 @@ python3 main.py**
 **The marks for every test is mentioned with the test definition.**
 
 *You will get marks corresponding to every test being passed.*
+
+Name: John Nguyen
+ID: 028380111
--- managementsystem.py
+++ managementsystem.py
@@ -24,6 +24,9 @@ class ManagementSystem:
         """
         Initialize a ManagementSystem object.
         """
+        self.employees = []
+        self.projects = []
+        self.tasks = []
         pass
 
     def add_employee(self, employee):
@@ -33,6 +36,7 @@ class ManagementSystem:
         Args:
             employee (Employee): The employee to be added.
         """
+        self.employees.append(employee)
         pass
 
     def remove_employee(self, emp_id):
@@ -42,6 +46,11 @@ class ManagementSystem:
         Args:
             emp_id (str): The ID of the employee to be removed.
         """
+        for n in range(0, len(self.employees)):
+            if self.employees[n].emp_id == emp_id:
+                del self.employees[n]
+                break
+                
         pass
 
     def add_project(self, project):
@@ -51,6 +60,7 @@ class ManagementSystem:
         Args:
             project (Project): The project to be added.
         """
+        self.projects.append(project)
         pass
 
     def add_task(self, task):
@@ -60,6 +70,7 @@ class ManagementSystem:
         Args:
             task (Task): The task to be added.
         """
+        self.tasks.append(task)
         pass
 
     def assign_employee_to_project(self, emp_id, project_id):
@@ -73,4 +84,14 @@ class ManagementSystem:
         Raises:
             ValueError: If employee or project is not found.
         """
+        try:
+            for e in range(0, len(self.employees)):
+                if self.employees[e].emp_id == emp_id:
+                    index_employee = e
+                    for p in range(0, len(self.projects)):
+                        if self.projects[p].project_id == project_id:
+                            self.projects[p].assign_employee(self.employees[e])
+        except ValueError:
+            print("Employee or Project could not be found")
+
         pass
